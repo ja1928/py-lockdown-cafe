a3d440a67a7f5c19b0a6c399c51b1e65e09f8c60
Łukasz Grzelak <lukaszgrzelak96@wp.pl>
2026-06-05 14:26:15 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,23 @@
-# write your code here
+import datetime
+
+from app.cafe import Cafe
+
+
+def go_to_cafe(friends: list, cafe: Cafe) -> str:
+    masks_to_buy = 0
+
+    for friend in friends:
+        if "vaccine" not in friend:
+            return "All friends should be vaccinated"
+
+        if friend["vaccine"]["expiration_date"] < datetime.date.today():
+            return "All friends should be vaccinated"
+
+    for friend in friends:
+        if friend["wearing_a_mask"] is False:
+            masks_to_buy += 1
+
+    if masks_to_buy > 0:
+        return f"Friends should buy {masks_to_buy} masks"
+
+    return f"Friends can go to {cafe.name}"
